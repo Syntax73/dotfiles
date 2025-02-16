467560d6ec40c1e5ee61a26ae51b99d1452e46cf
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2025-02-16 12:25:01 -0300

    Corrigida fonte do terminal

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,6 +6,7 @@ config = {
 	automatically_reload_config = true,
 	enable_tab_bar = false,
 	font_size = 11,
+	font = wezterm.font('JetBrainsMono Nerd Font'),
 	default_cursor_style = "BlinkingBar",
 	color_scheme = "Tokyo Night",
 	window_decorations = "RESIZE",
