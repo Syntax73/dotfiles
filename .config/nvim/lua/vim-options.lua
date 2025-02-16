068191f068553f8d133777a5bde6cf3ed5b6d013
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2025-02-16 12:26:22 -0300

    Configuração do nvim baseada nos videos 1 e 2 do typecraft

---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,12 @@
+vim.g.mapleader = " "
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.wo.number = true
+vim.o.relativenumber = true
+
+vim.o.termguicolors = true
+
