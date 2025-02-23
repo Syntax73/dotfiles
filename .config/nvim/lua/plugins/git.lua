8fa8eb7eaefb8ee2d8665676ef0b6878ad717a3c
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2025-02-23 12:42:10 -0300

    Melhorada config com git

---
+++ .config/nvim/lua/plugins/git.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "tpope/vim-fugitive"
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add = { text = "▎" },
+          change = { text = "▎" },
+          delete = { text = "_" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+          untracked = { text = "┆" },
+        },
+        signs_staged = {
+          add = { text = "▎" },
+          change = { text = "▎" },
+          delete = { text = "_" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+          untracked = { text = "┆" },
+        },
+        on_attach = function()
+          vim.keymap.set("n", "<leader>gs", ":Git<CR>", {})
+          vim.keymap.set("n", "<leader>gp", ":Git push<CR>", {})
+          vim.keymap.set("n", "<leader>gb", ":Gitsigns blame<CR>", {})
+          vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
+        end,
+      })
+    end,
+  },
+}
