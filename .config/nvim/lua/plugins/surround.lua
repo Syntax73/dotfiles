ff05c11dcf7988505625479592ad6f452504423d
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2025-11-09 11:53:28 -0300

    feature: added a lot of good stuff

---
+++ .config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,10 @@
+return {
+	"kylechui/nvim-surround",
+	version = "^3.1.7",
+	event = "VeryLazy",
+	config = function()
+		require("nvim-surround").setup({
+			-- Configuration here, or leave empty to use defaults
+		})
+	end,
+}
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -4,6 +4,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.opt.smartindent = true
 
 vim.wo.number = true
 vim.o.relativenumber = true
