f0f154f3cefcce7a44aa074df8a088e2632d0ed9
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2025-11-14 19:10:42 -0300

    refactor: change wezterm window padding and max fps

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,14 +3,21 @@ local wezterm = require("wezterm")
 config = wezterm.config_builder()
 
 config = {
-  automatically_reload_config = true,
-  enable_tab_bar = false,
-  font_size = 11,
-  font = wezterm.font('JetBrainsMono Nerd Font'),
-  default_cursor_style = "BlinkingBar",
-  color_scheme = "Tokyo Night",
-  window_decorations = "RESIZE",
-  window_background_opacity = 0.9
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	font_size = 11,
+	font = wezterm.font("JetBrainsMono Nerd Font"),
+	default_cursor_style = "BlinkingBar",
+	color_scheme = "Tokyo Night",
+	window_decorations = "RESIZE",
+	window_background_opacity = 0.9,
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	max_fps = 75,
 }
 
 return config
