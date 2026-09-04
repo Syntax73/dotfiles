c50abe6223c28d51756d2a7d4adce625aee37dbf
Syntax73 <45923733+Syntax73@users.noreply.github.com>
2026-09-04 18:53:59 -0300

    fix(nvim): lsp features working on zig lsp

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,29 @@
 return {
-  "romus204/tree-sitter-manager.nvim",
-  branch = 'main',
-  -- lazy = false,
-  -- build = ":TSUpdate",
-  config = function()
-    local configs = require("tree-sitter-manager")
+	"romus204/tree-sitter-manager.nvim",
+	branch = "main",
+	-- lazy = false,
+	-- build = ":TSUpdate",
+	config = function()
+		local configs = require("tree-sitter-manager")
 
-    configs.setup({
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "javascript",
-        "typescript",
-        "html",
-        "vue",
-        "zig",
-        "prisma",
-        "dart",
-      },
-      auto_install = false,
-      highlight = true,
-    })
-  end,
+		configs.setup({
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"javascript",
+				"typescript",
+				"html",
+				"vue",
+				"zig",
+				"prisma",
+				"dart",
+				"python",
+				"elixir",
+				"glsl",
+			},
+			auto_install = false,
+			highlight = true,
+		})
+	end,
 }
